7b89eeed0b246bb28a1c9409f349cb4e094e1da2
Benson Mwangi <bmwangi@SBA.LOCAL>
2024-07-19 15:06:16 +0300

    Deploying the appa on koyeb

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a^-!si_9u(5noyi=)l%uno@o!0kn95cba#2ajn)2lwdmi^^%e&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
